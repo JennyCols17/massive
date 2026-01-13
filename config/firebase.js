740953123348d107b68d9ee39a73be0380721582
JennyCols17 <mjmcolo@tip.edu.ph>
2026-01-13 14:20:43 +0800

    Fix: move firebase auth to environment variables

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,18 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // If we are on Render, use the Environment Variable
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    // If we are on your local computer, use the JSON file
+    serviceAccount = require("./serviceAccountKey.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const db = admin.firestore(); // This is your Database
-const auth = admin.auth();    // This is for User Management
-
-module.exports = { db, auth };
+const db = admin.firestore();
+module.exports = db;
